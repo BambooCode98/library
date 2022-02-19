8d159d47c6cc1ec535fb90be2be249ff5853cf8c
BambooCode98 <jw.thomas98@yahoo.com>
2022-02-19 00:53:59 -0600

    added more styles to the modal

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ const shelf = document.querySelector('body');
 const showbtn = document.querySelector('.unhideModal');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
 
 
 
 // console.log(form);
 console.log(library);
 
-function Book(title, author) {
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
+  this.pages = pages;
 }
 
 
@@ -22,25 +24,28 @@ function Book(title, author) {
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
+  //getting form values(inputs)
   const title = form.querySelector('input[id="title"]').value;
   const author = form.querySelector('input[id="author"]').value;
-  // console.log(`title: ${title}`, `author: ${author}`);
-  const newBook = new Book(title, author);
+  const pages = form.querySelector('input[id="pages"]').value;
+  const newBook = new Book(title, author, pages);
   library.push(newBook);
-  // console.log(newBook);
   console.log(library);
 
   
 
   //creating a book on the page
   const bookCard = document.createElement('div');
-  const bookTitle = document.createElement('h1');
+  const bookTitle = document.createElement('h2');
   const bookAuthor = document.createElement('h3');
+  const bookPages = document.createElement('h4');
   shelf.append(bookCard);
   bookCard.append(bookTitle);
-  bookTitle.textContent = newBook.title;
+  bookTitle.textContent = 'Title: ' + newBook.title;
   bookCard.append(bookAuthor);
-  bookAuthor.textContent = newBook.author;
+  bookAuthor.textContent = 'Author: ' + newBook.author;
+  bookCard.append(bookPages);
+  bookPages.textContent = 'Pages: ' + newBook.pages;
   
 
 })
